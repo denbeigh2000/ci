db37f84bcfa2d4af2dc5d590fc40d507c3c24934
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2024-03-31 16:02:42 -0700

    nix rustfmt: fix formatting and pass on generic pipeline args

--- nix/lib/rustfmt.nix
+++ nix/lib/rustfmt.nix
@@ -4,69 +4,67 @@ let
   inherit (lib) types;
 in
 {
-  options = {
-    rustfmt = {
-      package = lib.mkOption {
-        type = types.package;
-        default = pkgs.rustfmt;
-        description = ''
-          The derivation containing the `rustfmt` tool.
-        '';
-      };
+  options.rustfmt = {
+    package = lib.mkOption {
+      type = types.package;
+      default = pkgs.rustfmt;
+      description = ''
+        The derivation containing the `rustfmt` tool.
+      '';
+    };
 
-      checks = lib.mkOption {
-        description = ''
-          The set of `rustfmt` checks to run.
-        '';
+    checks = lib.mkOption {
+      description = ''
+        The set of `rustfmt` checks to run.
+      '';
 
-        default = { };
+      default = { };
 
-        type = types.attrsOf (types.submodule {
-          options = {
-            enable = lib.mkOption {
-              type = types.bool;
-              default = true;
-              description = ''
-                If true (the default) this rustfmt check will be run.
-              '';
-            };
-
-            edition = lib.mkOption {
-              type = types.enum [ "2015" "2018" "2021" ];
-              default = "2021";
-              description = ''
-                Edition of rust checks to run.
-              '';
-            };
-
-            src = lib.mkOption {
-              type = types.path;
-              description = ''
-                Path to the source directory to run on
-                (will have /**/*.rs appended)
-              '';
-            };
-
-            # TODO: put these in a lib?
-            dependsOn = lib.mkOption {
-              type = types.listOf types.str;
-              default = [ "wait-builds" ];
-              description = ''
-                Arbitrary list of steps to wait on. If not provided, waits until
-                all builds have been completed.
-              '';
-            };
-
-            timeoutMinutes = lib.mkOption {
-              type = types.int;
-              default = 5;
-              description = ''
-                Number of minutes to wait for release to be completed.
-              '';
-            };
+      type = types.attrsOf (types.submodule {
+        options = {
+          enable = lib.mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+              If true (the default) this `rustfmt` check will be run.
+            '';
           };
-        });
-      };
+
+          edition = lib.mkOption {
+            type = types.enum [ "2015" "2018" "2021" ];
+            default = "2021";
+            description = ''
+              Edition of rust checks to run.
+            '';
+          };
+
+          src = lib.mkOption {
+            type = types.path;
+            description = ''
+              Path to the source directory to run on
+              (will have /**/*.rs appended).
+            '';
+          };
+
+          # TODO: put these in a lib?
+          dependsOn = lib.mkOption {
+            type = types.listOf types.str;
+            default = [ "wait-builds" ];
+            description = ''
+              Arbitrary list of steps to wait on. If not provided, waits until
+              all builds have been completed.
+            '';
+          };
+
+          timeoutMinutes = lib.mkOption {
+            type = types.int;
+            default = 5;
+            description = ''
+              Number of minutes to wait for release to be completed.
+            '';
+          };
+        };
+      });
     };
   };
   config =
@@ -78,6 +76,8 @@ in
           config = {
             type = "command";
             label = ":rust: :broom: fmt ${key}";
+            depends_on = cfg.dependsOn;
+            timeout_in_minutes = cfg.timeoutMinutes;
           };
         };
       });
